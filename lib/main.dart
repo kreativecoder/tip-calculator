73fab91464358060adf6e048868bc9a4309251ef
Abiola Adebanjo <abiola.adebanjo@andela.com>
2019-06-12 00:46:40 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(TipCalculator());
+
+class TipCalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tip Calculator'),
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
