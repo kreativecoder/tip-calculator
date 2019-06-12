57e7cf2ad0c731d4af48ce0bd7520e2ebd3821d3
Abiola Adebanjo <abiola.adebanjo@andela.com>
2019-06-12 02:55:07 +0100

    Layout crafting

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,12 @@ class TipCalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color(0xff62727b),
         appBar: AppBar(
           title: Text('Tip Calculator'),
+          backgroundColor: Color(0xff102027),
         ),
+        body: MainPage(),
       ),
     );
   }
@@ -23,6 +26,176 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 0.0, top: 30.0, right: 0.0, bottom: 10.0),
+                  child: Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff62727b),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 0.0, top: 0.0, right: 0.0, bottom: 30.0),
+                  child: Text(
+                    'Per Buddy',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      color: Color(0xff62727b),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Total Damage (\$)',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Color(0xffffffff),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: TextField(
+                  decoration:
+                      InputDecoration(border: InputBorder.none, hintText: '0'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Generous Tip',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: FlatButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/subtract.png',
+                            width: 75,
+                            height: 75,
+                          )),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          '20%',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: FlatButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/add.png',
+                            width: 75,
+                            height: 75,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Buddies',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Color(0xffffffff),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: FlatButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/subtract.png',
+                            width: 75,
+                            height: 75,
+                          )),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          '4',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: FlatButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/add.png',
+                            width: 75,
+                            height: 75,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
